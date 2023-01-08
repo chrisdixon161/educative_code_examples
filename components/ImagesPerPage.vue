b6590b2bde7b90fa81cc7e9003f198600f29770e
chris dixon <chrisdixon161@googlemail.com>
2023-01-08 11:12:03 +0000

    images per page

---
+++ components/ImagesPerPage.vue
@@ -0,0 +1,11 @@
+<script setup>
+const imagesPerPage = useImagesPerPage();
+</script>
+<template>
+	<select v-model="imagesPerPage">
+		<option disabled value="">Please select one</option>
+		<option>20</option>
+		<option>50</option>
+		<option>100</option>
+	</select>
+</template>
--- pages/index.vue
+++ pages/index.vue
@@ -43,6 +43,19 @@ function handleTagClicked(tag) {
 			</button>
 		</section>
 	</header>
+	<section class="options_wrapper">
+		<div class="menu_options">
+			<ImagesPerPage />
+		</div>
+		<div class="pixabay_credit">
+			<span>Images by: </span>
+			<img
+				class="pixabay_credit_image"
+				src="images/logo.svg"
+				alt="pixabay logo"
+			/>
+		</div>
+	</section>
 	<ul class="images">
 		<li v-for="image in displayImages" :key="image.id">
 			<img class="img_preview" :src="image.webformatURL" />
@@ -58,3 +71,87 @@ function handleTagClicked(tag) {
 		<p>page {{ currentPageNumber }} of {{ numberOfPages }}</p>
 	</footer>
 </template>
+
+<style>
+.colors_wrapper {
+	display: flex;
+	max-width: 50%;
+	flex-wrap: wrap;
+}
+.text_color_option {
+	cursor: pointer;
+}
+.color_option {
+	border-radius: 100%;
+	width: 20px;
+	height: 20px;
+	cursor: pointer;
+	display: block;
+}
+.search_header {
+	display: flex;
+	flex-direction: column;
+	align-items: center;
+}
+.search {
+	margin: 1rem;
+	padding: 10px;
+	border-radius: 5px;
+	width: 50%;
+}
+.search_tags {
+	display: flex;
+	flex-direction: row;
+	flex-wrap: wrap;
+	justify-content: center;
+}
+.options_wrapper {
+	display: flex;
+	align-items: center;
+	margin: 1rem 0;
+}
+
+.menu_options {
+	display: flex;
+}
+
+.pixabay_credit {
+	display: flex;
+	justify-content: flex-end;
+}
+
+.pixabay_credit_image {
+	max-width: 15%;
+	margin-left: 10px;
+}
+
+.images {
+	width: 100%;
+	display: flex;
+	flex-direction: row;
+	flex-wrap: wrap;
+	justify-content: center;
+}
+
+ul {
+	padding: 0;
+	margin: 0;
+}
+
+li {
+	list-style: none;
+	margin: 10px;
+}
+
+.img_preview {
+	max-height: 200px;
+	cursor: pointer;
+}
+button {
+	border: 1px lightslategray solid;
+	background: none;
+	margin: 5px 2px;
+	border-radius: 3px;
+	cursor: pointer;
+}
+</style>
